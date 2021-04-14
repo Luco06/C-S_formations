6d3fefffd9108e8163b8187f46c95ae3651a4353
Luco06 <luc-olivier.yohan@hotmail.fr>
2021-04-14 20:31:54 +0200

    add js function

--- script.js
+++ script.js
@@ -1,39 +1,39 @@
-function openCard1(){
-    document.getElementById("card_body1").style.height="auto";
-    document.getElementById("card_body1").style.opacity=1;
-    document.getElementById("down1").style.visibility="hidden";
-    document.getElementById("up1").style.visibility="visible";
-}
-function closeCard1(){
-    document.getElementById("card_body1").style.height=0;
-    document.getElementById("card_body1").style.opacity=0;
-    document.getElementById("down1").style.visibility="visible";
-    document.getElementById("up1").style.visibility="hidden";
+let card1 = document.querySelector('.card1');
+let card2 = document.querySelector('.card2');
+let card3 = document.querySelector('.card3');
 
-}
-function openCard2(){
-    document.getElementById("card_body2").style.height="auto";
-    document.getElementById("card_body2").style.opacity=1;
-    document.getElementById("down2").style.visibility="hidden";
-    document.getElementById("up2").style.visibility="visible";
-}
-function closeCard2(){
-    document.getElementById("card_body2").style.height=0;
-    document.getElementById("card_body2").style.opacity=0;
-    document.getElementById("down2").style.visibility="visible";
-    document.getElementById("up2").style.visibility="hidden";
+let bcard1 = document.querySelector('.card_body1');
+let bcard2 = document.querySelector('.card_body2');
+let bcard3 = document.querySelector('.card_body3');
 
+function visible1(){
+    if(bcard1.classList.contains('visible')){
+        bcard1.classList.remove('visible')
+    }else{
+        bcard1.classList.add('visible')
+    }
 }
-function openCard3(){
-    document.getElementById("card_body3").style.height="auto";
-    document.getElementById("card_body3").style.opacity=1;
-    document.getElementById("down3").style.visibility="hidden";
-    document.getElementById("up3").style.visibility="visible";
+function visible2(){
+    if(bcard2.classList.contains('visible')){
+        bcard2.classList.remove('visible')
+    }else{
+        bcard2.classList.add('visible')
+    }
 }
-function closeCard3(){
-    document.getElementById("card_body3").style.height=0;
-    document.getElementById("card_body3").style.opacity=0;
-    document.getElementById("down3").style.visibility="visible";
-    document.getElementById("up3").style.visibility="hidden";
+function visible3(){
+    if(bcard3.classList.contains('visible')){
+        bcard3.classList.remove('visible')
+    }else{
+        bcard3.classList.add('visible')
+    }
+}
 
-}
+card1.addEventListener('click', () =>{
+    visible1();
+})
+card2.addEventListener('click', () =>{
+    visible2();
+})
+card3.addEventListener('click', () =>{
+    visible3();
+})
