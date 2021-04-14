4829c825fe3d31c60921c33692137fc322f1e176
Luco06 <luc-olivier.yohan@hotmail.fr>
2021-04-14 16:04:13 +0200

    add correction

--- script.js
+++ script.js
@@ -1,11 +1,39 @@
-function openCard(){
-    document.querySelector(".card_body").style.height="auto";
-    document.querySelector(".card_body").style.opacity=1;
-    document.querySelector(".down").style.visibility="hidden";
-    document.querySelector(".up").style.visibility="visible";
+function openCard1(){
+    document.getElementById("card_body1").style.height="auto";
+    document.getElementById("card_body1").style.opacity=1;
+    document.getElementById("down1").style.visibility="hidden";
+    document.getElementById("up1").style.visibility="visible";
 }
-function closeCard(){
-    document.querySelector(".card_body").style.height=0;
-    document.querySelector(".card_body").style.opacity=0;
+function closeCard1(){
+    document.getElementById("card_body1").style.height=0;
+    document.getElementById("card_body1").style.opacity=0;
+    document.getElementById("down1").style.visibility="visible";
+    document.getElementById("up1").style.visibility="hidden";
+
+}
+function openCard2(){
+    document.getElementById("card_body2").style.height="auto";
+    document.getElementById("card_body2").style.opacity=1;
+    document.getElementById("down2").style.visibility="hidden";
+    document.getElementById("up2").style.visibility="visible";
+}
+function closeCard2(){
+    document.getElementById("card_body2").style.height=0;
+    document.getElementById("card_body2").style.opacity=0;
+    document.getElementById("down2").style.visibility="visible";
+    document.getElementById("up2").style.visibility="hidden";
+
+}
+function openCard3(){
+    document.getElementById("card_body3").style.height="auto";
+    document.getElementById("card_body3").style.opacity=1;
+    document.getElementById("down3").style.visibility="hidden";
+    document.getElementById("up3").style.visibility="visible";
+}
+function closeCard3(){
+    document.getElementById("card_body3").style.height=0;
+    document.getElementById("card_body3").style.opacity=0;
+    document.getElementById("down3").style.visibility="visible";
+    document.getElementById("up3").style.visibility="hidden";
 
 }
